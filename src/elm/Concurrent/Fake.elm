e03289f1eb26fc64e54796553b710b8744d562d6
andrewMacmurray <a.macmurray@icloud.com>
2023-05-16 18:27:39 +0100

    adds errors to fake

--- src/elm/Concurrent/Fake.elm
+++ src/elm/Concurrent/Fake.elm
@@ -3,13 +3,13 @@ module Concurrent.Fake exposing (runExample)
 -- Task
 
 
-type Task a
-    = State (Int -> ( Int, Task_ a ))
+type Task x a
+    = State (Int -> ( Int, Task_ x a ))
 
 
-type Task_ a
-    = Pending (List Definition_) (Task a)
-    | Done a
+type Task_ x a
+    = Pending (List Definition_) (Task x a)
+    | Done (Result x a)
 
 
 type alias Definition_ =
@@ -24,16 +24,16 @@ type alias Definition_ =
 
 type alias Definition a =
     { function : String
-    , expect : a
+    , expect : Result String a
     }
 
 
-define : Definition a -> Task a
+define : Definition a -> Task String a
 define a =
     State
         (\s ->
             ( s + 1
-            , Pending [ { id = s, function = a.function } ] (succeed a.expect)
+            , Pending [ { id = s, function = a.function } ] (fromResult a.expect)
             )
         )
 
@@ -42,7 +42,7 @@ define a =
 -- Ops
 
 
-map : (a -> b) -> Task a -> Task b
+map : (a -> b) -> Task x a -> Task x b
 map f (State run) =
     State
         (\s ->
@@ -54,17 +54,17 @@ map f (State run) =
         )
 
 
-mapTask : (a -> b) -> Task_ a -> Task_ b
+mapTask : (a -> b) -> Task_ x a -> Task_ x b
 mapTask f task =
     case task of
         Pending defs next ->
             Pending defs (map f next)
 
         Done a ->
-            Done (f a)
+            Done (Result.map f a)
 
 
-map2 : (a -> b -> c) -> Task a -> Task b -> Task c
+map2 : (a -> b -> c) -> Task x a -> Task x b -> Task x c
 map2 f (State run1) (State run2) =
     State
         (\s ->
@@ -79,28 +79,38 @@ map2 f (State run1) (State run2) =
         )
 
 
-mapTask2 : (a -> b -> c) -> Task_ a -> Task_ b -> Task_ c
+mapTask2 : (a -> b -> c) -> Task_ x a -> Task_ x b -> Task_ x c
 mapTask2 f task1 task2 =
     case ( task1, task2 ) of
         ( Pending defs1 next1, Pending defs2 next2 ) ->
             Pending (defs1 ++ defs2) (map2 f next1 next2)
 
         ( Pending defs next1, Done b ) ->
-            Pending defs (map2 f next1 (succeed b))
+            Pending defs (map2 f next1 (fromResult b))
 
         ( Done a, Pending defs next2 ) ->
-            Pending defs (map2 f (succeed a) next2)
+            Pending defs (map2 f (fromResult a) next2)
 
         ( Done a, Done b ) ->
-            Done (f a b)
+            Done (Result.map2 f a b)
 
 
-succeed : a -> Task a
+succeed : a -> Task x a
 succeed a =
-    State (\s -> ( s, Done a ))
+    fromResult (Ok a)
 
 
-andThen : (a -> Task b) -> Task a -> Task b
+fail : x -> Task x a
+fail x =
+    fromResult (Err x)
+
+
+fromResult : Result x a -> Task x a
+fromResult res =
+    State (\s -> ( s, Done res ))
+
+
+andThen : (a -> Task x b) -> Task x a -> Task x b
 andThen f (State run) =
     State
         (\s ->
@@ -111,7 +121,12 @@ andThen f (State run) =
                 (State run_) =
                     case a of
                         Done a_ ->
-                            f a_
+                            case a_ of
+                                Ok a__ ->
+                                    f a__
+
+                                Err e ->
+                                    fail e
 
                         Pending defs next ->
                             State (\s__ -> ( s__, Pending defs (next |> andThen f) ))
@@ -120,35 +135,41 @@ andThen f (State run) =
         )
 
 
-addDefs : List Definition_ -> Task a -> Task a
-addDefs defs (State toTask) =
-    State
-        (\model ->
-            let
-                ( model1, task_ ) =
-                    toTask model
-            in
-            ( model1
-            , case task_ of
-                Done a ->
-                    Done a
-
-                Pending defs1 next ->
-                    Pending (defs ++ defs1) next
-            )
-        )
-
-
-andThenDo : Task b -> Task a -> Task b
+andThenDo : Task x b -> Task x a -> Task x b
 andThenDo s2 s1 =
     s1 |> andThen (\_ -> s2)
+
+
+onError : (x -> Task y a) -> Task x a -> Task y a
+onError f (State run) =
+    State
+        (\s ->
+            let
+                ( s_, a ) =
+                    run s
+
+                (State run_) =
+                    case a of
+                        Done a_ ->
+                            case a_ of
+                                Ok a__ ->
+                                    succeed a__
+
+                                Err e ->
+                                    f e
+
+                        Pending defs next ->
+                            State (\s__ -> ( s__, Pending defs (next |> onError f) ))
+            in
+            run_ s_
+        )
 
 
 
 -- Eval
 
 
-eval : Task a -> Int -> ( Int, a )
+eval : Task x a -> Int -> ( Int, Result x a )
 eval (State run) n =
     case run n of
         ( n_, Done a ) ->
@@ -169,7 +190,14 @@ eval (State run) n =
 create a =
     define
         { function = "create"
-        , expect = a
+        , expect = Ok a
+        }
+
+
+error =
+    define
+        { function = "error"
+        , expect = Err "error"
         }
 
 
@@ -185,12 +213,22 @@ example =
         )
         |> andThenDo
             (create "foo"
-                |> andThenDo (create "bar")
+                |> andThenDo
+                    (error
+                        |> onError (\_ -> error)
+                        |> onError (\_ -> error)
+                        |> onError (\_ -> create "bar")
+                    )
                 |> andThenDo
                     (create "bar"
                         |> andThenDo (create "bar")
                         |> andThenDo (create "bar")
-                        |> andThenDo (create "bar" |> andThenDo (create "bar"))
+                        |> andThenDo
+                            (create "bar"
+                                |> andThenDo (create "bar")
+                                |> andThenDo error
+                                |> onError (\_ -> create "bar")
+                            )
                         |> andThenDo (create "bar")
                     )
             )
