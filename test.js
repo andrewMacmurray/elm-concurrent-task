e3a79bcd876b77209d838e49330dd86de647e015
andrewMacmurray <a.macmurray@icloud.com>
2023-07-01 17:07:29 +0100

    adds raw node equivalent of sequence

--- test.js
+++ test.js
@@ -1,15 +1,27 @@
 import axios from "axios";
 
-main();
+doBatch();
+// doSequence();
 
-function main() {
-  Promise.all(
-    Array.from({ length: 800 }).map(() =>
-      axios
-        .get("http://localhost:4000/wait-then-respond/0")
-        .then((x) => x.data.message)
-    )
-  )
+function doBatch() {
+  Promise.all(Array.from({ length: 800 }).map((_, i) => doRequest(i)))
     .then((xs) => xs.join(","))
     .then(console.log);
+}
+
+function doSequence() {
+  Array.from({ length: 800 })
+    .reduce(
+      (acc, _, i) =>
+        acc.then((res) => doRequest(i).then((res2) => `${res},${res2}`)),
+      Promise.resolve("start")
+    )
+    .then(console.log);
+}
+
+function doRequest(i) {
+  console.log(`STARTING - ${i}`);
+  return axios
+    .get("http://localhost:4000/wait-then-respond/0")
+    .then((x) => x.data.message);
 }
