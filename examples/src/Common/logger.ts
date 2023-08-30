50321386ca9bb6791c6c290ba405c6bd87f3c1b7
andrewMacmurray <a.macmurray@icloud.com>
2023-08-30 17:58:07 +0100

    use pino for logger

--- examples/src/Common/logger.ts
+++ examples/src/Common/logger.ts
@@ -1,4 +1,8 @@
-import chalk from "chalk";
+import pino from "pino";
+import pretty from "pino-pretty";
+
+const stream = pretty({ colorize: true });
+const logger = pino({}, stream);
 
 interface Options {
   level: Level;
@@ -18,39 +22,14 @@ export function tasks() {
 export function log(options: Options): void {
   switch (options.level) {
     case "DEBUG":
-      return debug(options.message);
+      return logger.debug(options.message);
     case "INFO":
-      return info(options.message);
+      return logger.info(options.message);
     case "ERROR":
-      return error(options.message);
+      return logger.error(options.message);
     case "WARN":
-      return warn(options.message);
+      return logger.warn(options.message);
     default:
       throw new Error(`Unrecognized Log Level: ${options}`);
   }
-}
-
-export function debug(message: string): void {
-  console.debug(formatMessage(chalk.magenta, "DEBUG", message));
-}
-
-export function info(message: string): void {
-  console.info(formatMessage(chalk.cyan, "INFO", message));
-}
-
-export function warn(message: string): void {
-  console.warn(formatMessage(chalk.yellow, "WARN", message));
-}
-
-export function error(message: string): void {
-  console.error(formatMessage(chalk.red, "ERROR", message));
-}
-
-function formatMessage(
-  toColor: (string) => string,
-  level: Level,
-  message: string
-): string {
-  const logPrefix = `[${level}]`.padEnd(7, " ");
-  return `${toColor(logPrefix)} ${toColor(":")} ${message}`;
 }
