691c599d9cf4db818e6bd6c5859e4374ce86e12d
andrewMacmurray <a.macmurray@icloud.com>
2023-08-30 18:18:13 +0100

    remove unused

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -1,31 +1,11 @@
 import { Elm } from "./Worker.elm";
-import crypto from "node:crypto";
 import * as Tasks from "../../src/runner";
-import readline from "node:readline/promises";
 import * as S3 from "./Aws/s3";
 import * as SQS from "./Aws/sqs";
 import * as SNS from "./Aws/sns";
 import * as Env from "./Common/env";
 import * as Uuid from "./Common/uuid";
 import * as Logger from "./Common/logger";
-
-// Tasks
-
-const CustomTasks = {
-  slowInt: (i) => waitRandom().then(() => i),
-  "console:time": (label) => console.time(label),
-  "console:timeEnd": (label) => console.timeEnd(label),
-};
-
-function waitRandom() {
-  return wait(crypto.randomInt(0, 500));
-}
-
-function wait(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
 
 // App
 
@@ -39,7 +19,6 @@ Tasks.register({
     ...Env.tasks(),
     ...Uuid.tasks(),
     ...Logger.tasks(),
-    ...CustomTasks,
   },
   ports: {
     send: ports.send,
@@ -47,30 +26,3 @@ Tasks.register({
   },
   debug: { taskStart: false },
 });
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-// ask();
-// fireMany();
-
-// ports.sendResult.subscribe((result) => {
-//   console.log(result);
-//   ask();
-// });
-
-function fireMany() {
-  for (let i = 0; i < 10; i++) {
-    waitRandom().then(() => {
-      ports.fireMany.send(i);
-    });
-  }
-}
-
-function ask() {
-  rl.question("enter an attempt id: ").then((id) => {
-    ports.manualEnter.send(id);
-  });
-}
