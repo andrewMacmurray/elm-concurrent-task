9a3cada9ef6600f0d61dc96b8a15af2de8e3f3bf
Andrew MacMurray <a.macmurray@icloud.com>
2026-01-01 17:06:18 +0000

    bump dev deps, use latest vite with logger workaround
    Vite prints an elm import warning, so the custom logger works around this

--- examples/dom-operations/vite.config.js
+++ examples/dom-operations/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import elmPlugin from "vite-plugin-elm";
+import { customLogger } from "../util/vite-logger";
 
 export default defineConfig({
   root: "./src",
   plugins: [elmPlugin()],
+  customLogger,
 });
